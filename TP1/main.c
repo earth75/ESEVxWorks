09b5623914c3a5b363c67dae21bf82015b912043
earth75 <alexis.tetar@gmail.com>
2015-11-15 13:02:33 +0100

    Comment the main file

--- TP1/main.c
+++ TP1/main.c
@@ -1,3 +1,10 @@
+/* * * * * * * * *   TP1/main.c    * * * * * * * * * *\
+   This is the test program we used for the driver
+        It read the values from each axis
+    and sends them tu the UART port at 115200 baud
+\* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
 
 #include <stdio.h>
 #include <string.h>
@@ -41,22 +48,24 @@ void init(void)
 		printf("Erreur intEnable\n");
 		taskDelete(0);
 	}
+	
 	//initialisation de l'accéléro
-	//axe Z
+	//le nom est /axeX pour ouvrir l'axe X par défaut
+	//toute combinaison est acceptée, mais "/axe" est déconseillée
 	accD = open("/axeX", O_RDONLY, 0);
 	if (accD == -1) {printf("error on open\n"); return;}
 	else printf("InitAcc OK\n");
 	
 	//initialisation du port série 2
-	// 9600baud
-	//8bits de donée
-	//1 bit de stop
-	//pas de controle
-	
+	  // 115200 baud
+ 	  // 8bits de donée
+	  // 1 bit de stop
+	  // pas de controle
 	fd = open("/tyCo/1", O_RDWR, 0x777);
 	if (fd == -1) {printf("error on open\n"); return;}
 	else {ioctl(fd, FIOBAUDRATE, 115200);
 	printf("Init OK\n");
+	
 	return;}
 }
 
@@ -69,13 +78,13 @@ void main(void)
 	
 	while(1)
 	{
-		semTake(TIMsynch, WAIT_FOREVER);				//synch with timer (1Hz)
-		ioctl(accD, 0, 'X');
-		read(accD, bufferX, 10);
-		ioctl(accD, 0, 'Y');
-		read(accD, bufferY, 10);
-		ioctl(accD, 0, 'Z');
-		read(accD, bufferZ, 10);
+		semTake(TIMsynch, WAIT_FOREVER);		//synch with timer (1Hz)
+		ioctl(accD, 0, 'X');				//select X
+		read(accD, bufferX, 10);			//read and store X
+		ioctl(accD, 0, 'Y');				//select Y
+		read(accD, bufferY, 10);			//read and store Y
+		ioctl(accD, 0, 'Z');				//select Z
+		read(accD, bufferZ, 10);			//read and store Z
 		varX = (float) atoi(bufferX);
 		sprintf(bufferX,"%1.*f",precision, varX/16384);	//precision = nb de chiffres apres la virgule
 		varY = (float) atoi(bufferY);
@@ -89,7 +98,7 @@ void main(void)
 		strcat(buffer,", ");
 		strcat(buffer,bufferZ);
 		strcat(buffer, ")\n\r");
-		write(fd, buffer,strlen(buffer));
+		write(fd, buffer,strlen(buffer));		//write to UART port
 	}
 }
 
